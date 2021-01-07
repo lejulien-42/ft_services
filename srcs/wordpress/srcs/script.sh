d5462709ea8ba3cb3303ec7a11540ad9749a798d
leulien-42 <lejulien@student.42.fr>
2021-01-07 17:58:17 +0100

    fixing a bit of mysql

--- srcs/wordpress/srcs/script.sh
+++ srcs/wordpress/srcs/script.sh
@@ -6,15 +6,30 @@
 #    By: lejulien <lejulien@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/01/05 16:49:28 by lejulien          #+#    #+#              #
-#    Updated: 2021/01/07 11:47:53 by user42           ###   ########.fr        #
+#    Updated: 2021/01/07 14:38:35 by lejulien         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 rc-update add nginx default
 rc-update add php-fpm7 default
 
-rc-service nginx restart
-rc-service php-fpm7 restart
+rc-service nginx stop
+rc-service php-fpm7 stop
+rc-service nginx start
+rc-service php-fpm7 start
+rc-service mariadb start
+
+rc-update add mariadb default
+
+mysql_secure_installation -y
+
+mysql -u root -bse "
+CREATE DATABASE IF NOT EXISTS wordpress_db;
+GRANT ALL ON wordpress_db.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'toor';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost';
+FLUSH PRIVILEGES;"
+
 
 nginx -g "pid /tmp/nginx.pid; daemon off;"
 /bin/ash
