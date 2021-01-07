27ec9816dcfab039709f6f92491b5cbb0b97cef5
lejulien-42 <lejulien@student.42.fr>
2021-01-07 13:40:24 +0100

    fwfe

--- srcs/wordpress/srcs/script.sh
+++ srcs/wordpress/srcs/script.sh
@@ -6,9 +6,15 @@
 #    By: lejulien <lejulien@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/01/05 16:49:28 by lejulien          #+#    #+#              #
-#    Updated: 2021/01/05 17:08:18 by lejulien         ###   ########.fr        #
+#    Updated: 2021/01/07 11:47:53 by user42           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
+rc-update add nginx default
+rc-update add php-fpm7 default
+
+rc-service nginx restart
+rc-service php-fpm7 restart
 
 nginx -g "pid /tmp/nginx.pid; daemon off;"
 /bin/ash
