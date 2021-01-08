e24c75f0d430c3ea4df4a435d09244f2d664d39a
leulien-42 <lejulien@student.42.fr>
2021-01-08 17:32:06 +0100

    wordpress retard

--- srcs/wordpress/srcs/script.sh
+++ srcs/wordpress/srcs/script.sh
@@ -6,22 +6,18 @@
 #    By: lejulien <lejulien@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/01/05 16:49:28 by lejulien          #+#    #+#              #
-#    Updated: 2021/01/08 14:24:57 by lejulien         ###   ########.fr        #
+#    Updated: 2021/01/08 17:16:24 by lejulien         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 rc-update add nginx default
 rc-update add php-fpm7 default
+rc-update add mariadb default
 
 rc-service nginx stop
 rc-service php-fpm7 stop
-rc-service nginx start
 rc-service php-fpm7 start
 rc-service mariadb restart
-
-rc-update add mariadb default
-
-mysql_secure_installation -y
 
 mysql -u root -bse "
 CREATE DATABASE IF NOT EXISTS wordpress_db;
@@ -32,5 +28,3 @@ FLUSH PRIVILEGES;"
 
 
 nginx -g "pid /tmp/nginx.pid; daemon off;"
-/bin/ash
-
