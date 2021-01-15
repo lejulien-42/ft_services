187148cebadc06d9a9af9eee77f338e3d1c26b52
lejulien-42 <lejulien@student.42.fr>
2021-01-15 12:32:24 +0100

    adding mysql

---
+++ srcs/mariadb/srcs/init.sh
@@ -0,0 +1,30 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    init.sh                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: lejulien <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/01/15 12:13:44 by lejulien          #+#    #+#              #
+#    Updated: 2021/01/15 12:31:16 by lejulien         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+
+openrc
+touch /run/openrc/softlevel
+
+/etc/init.d/mariadb setup
+sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
+service mariadb restart
+
+mysql -u root << EOF
+	CREATE DATABASE wordpress;
+	CREATE USER 'wp_user'@'%' IDENTIFIED BY '$WP_ADMIN_PASS';
+	GRANT ALL ON wordpress.* TO 'wp_user'@'%' IDENTIFIED BY '$WP_USER_PASS' WITH GRANT OPTION;
+	CREATE USER 'admin'@'%' IDENTIFIED BY '$ADMIN_PASS';
+	GRANT ALL ON *.* TO 'admin'@'%' IDENTIFIED BY '$ADMIN_PASS' WITH GRANT OPTION;
+	FLUSH PRIVILEGES;
+EOF
+
+ash
