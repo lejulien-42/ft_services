9963e6211ec74fec62ca863189acb51511e8ca26
lejulien-42 <re4chan@gmail.com>
2021-01-25 10:53:23 -0800

    minikube

--- setup.sh
+++ setup.sh
@@ -6,11 +6,19 @@
 #    By: lejulien <lejulien@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/01/25 11:57:54 by lejulien          #+#    #+#              #
-#    Updated: 2021/01/25 11:58:51 by lejulien         ###   ########.fr        #
+#    Updated: 2021/01/25 09:04:02 by lejulien         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-# On first install only
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+minikube delete 2> /dev/null
+sudo -R $USER $HOME/.minikube; chmod -R u+w+r $HOME/.minikube
+
+printf "$USER\n$USER" | sudo -S chmod 666 /var/run/docker.sock
+minikube start -vm-driver=docker
+
+minikube addons enable logviewer
+minikube addons enable metric-server
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml > /dev/null
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml > /dev/null
+kubectl apply -f srcs/metallb-config.yaml
