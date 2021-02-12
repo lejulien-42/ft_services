f000683309f355ae5dc3519dbb1dd049801fb0ce
lejulien-42 <lejulien@student.42.fr>
2021-02-12 16:24:00 +0100

    starting repolishing

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mag=$'\e[1;35m'
+end=$'\e[0m'
+
+printf "${mag}cleaning...${end}\n"
+docker system prune -f
+minikube delete
+sudo pkill nginx
+sudo pkill mysqld
+
+printf "${mag}giving rights to docker...${end}\n"
+printf "${mag}$USER\n$USER" | sudo -S chmod 666 /var/run/docker.sock
+
+printf "${mag}start minikube...${end}\n"
+./srcs/minikube start --vm-driver=docker
+
+printf "${mag}starting eval...${end}\n"
+eval $(./srcs/minikube -p minikube docker-env)
+
+printf "${mag}enabling metallb...${end}\n"
+minikube addons enable metallb
+
