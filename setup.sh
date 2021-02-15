cd5eed8b56835a854a8bf3185e562e00739ca8e0
lejulien-42 <lejulien@student.42.fr>
2021-02-15 15:25:28 +0100

    poishing and make it work

--- setup.sh
+++ setup.sh
@@ -3,6 +3,22 @@
 mag=$'\e[1;35m'
 end=$'\e[0m'
 
+echo ${mag}
+
+printf '  ______     __                                                    __                               '	;echo " "
+printf ' /      \   |  \                                                  |  \                              '	;echo " "
+printf '|  ######\ _| ##_           _______   ______    ______  __     __  \##  _______   ______    _______ '	;echo " "
+printf '| ##_  \##|   ## \         /       \ /      \  /      \|  \   /  \|  \ /       \ /      \  /       \'	;echo " "
+printf '| ## \     \######        |  #######|  ######\|  ######\\##\  /  ##| ##|  #######|  ######\|  #######'	;echo " "
+printf '| ####      | ## __        \##    \ | ##    ##| ##   \## \##\  ## | ##| ##      | ##    ## \##    \ '	;echo " "
+printf '| ##        | ##|  \       _\######\| ########| ##        \## ##  | ##| ##_____ | ######## _\######\'	;echo " "
+printf '| ##         \##  ##______|       ## \##     \| ##         \###   | ## \##     \ \##     \|       ##'	;echo " "
+printf ' \##          \####|      \\\#######   \####### \##          \#     \##  \#######  \####### \####### '	;echo " "
+printf '                    \######                                            by : lejulien                '	;echo " ";echo " "
+
+
+
+
 printf "${mag}cleaning...${end}\n"
 docker system prune -f
 minikube delete
@@ -15,6 +31,13 @@ printf "${mag}$USER\n$USER" | sudo -S chmod 666 /var/run/docker.sock
 printf "${mag}start minikube...${end}\n"
 ./srcs/minikube start --vm-driver=docker
 
+printf "${mag}setting up ip range...\n${end}"
+last=$(minikube ip | sed 's!.*\.!!')
+last=$((last+1))
+ip=$(minikube ip | sed "s:[^.]*$:$last:")
+line="          - $ip-$ip"
+sed -i "13s/.*/$line/" ./srcs/metallb/metallb.yaml
+
 printf "${mag}starting eval...${end}\n"
 eval $(./srcs/minikube -p minikube docker-env)
 
@@ -26,3 +49,8 @@ printf "${mag}enabling nginx...${end}\n"
 eval $(./srcs/minikube -p minikube docker-env)
 docker build -t my_nginx ./srcs/nginx
 kubectl apply -f ./srcs/nginx/nginx.yaml
+
+printf "${mag}enabling mysql...${end}\n"
+docker build -t my_mysql ./srcs/mariadb
+
+printf "${mag}build finished !!\n\n    - nginx ip : http://$ip:80\n$end"
--- srcs/nginx/start.sh
+++ srcs/nginx/start.sh
@@ -1,3 +1,3 @@
 sudo pkill nginx
-sudo docker rmi -f my_nginx; sudo docker build -t my_nginx . ; sudo docker run -d -p 80:80 -p 443:443 my_nginx
+sudo docker system prune;sudo docker rmi -f my_nginx; sudo docker build -t my_nginx . ; sudo docker run -d -p 80:80 -p 443:443 my_nginx
 
