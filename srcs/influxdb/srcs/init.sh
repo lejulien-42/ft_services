d777634748d50347e5fd80fbb45f8d27b9fd9cd1
lejulien-42 <lejulien@student.42.fr>
2021-02-26 21:09:03 +0100

    finaly run influxdb

--- srcs/influxdb/srcs/init.sh
+++ srcs/influxdb/srcs/init.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-telegraf & influxd run -config /etc/influxdb.conf
+touch /run/openrc/softlevel
+openrc boot
+influxd 2>$HOME/my_log_file
+influx -execute "CREATE DATABASE influxdb"
+influx -execute "CREATE USER lejulien WITH PASSWORD 'lejulien'"
+influx -execute "GRANT ALL ON influxdb TO lejulien"
+telegraf
+tail -f /dev/null
