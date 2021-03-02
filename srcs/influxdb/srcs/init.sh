80beee9f6864ebef57c0d339816cd650903366de
lejulien-42 <lejulien@student.42.fr>
2021-03-02 16:08:46 +0100

    fixing influxdb

--- srcs/influxdb/srcs/init.sh
+++ srcs/influxdb/srcs/init.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-influxd && sleep 5 \
-	influx -execute "CREATE DATABASE influxdb" && \
-	influx -execute "CREATE USER lejulien WITH PASSWORD 'lejulien'" && \
-	influx -execute "GRANT ALL ON influxdb TO lejulien"
+apk add openrc
+
+openrc boot
+
+
+service influxdb start
+
+sleep 10
+
+influx -execute "CREATE DATABASE influxdb" && \
+influx -execute "CREATE USER lejulien WITH PASSWORD 'lejulien'" && \
+influx -execute "GRANT ALL ON influxdb TO lejulien"
+
+tail -f /dev/null
